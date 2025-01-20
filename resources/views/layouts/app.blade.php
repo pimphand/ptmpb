6b2a17f09a913eb6b342028e5c89dda687864887
pimphand <faisaldwiki69@gmail.com>
2025-01-20 16:23:19 +0700

    `Update og:image and twitter:image meta tags to use asset() helper`

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <meta property="og:title" content="Mandalika Putra Bersama - Toko Oli Terpercaya di Garut">
     <meta property="og:description"
         content="Mandalika Putra Bersama menyediakan oli berkualitas untuk kendaraan Anda di Garut. Kunjungi kami untuk produk terbaik!">
-    <meta property="og:image" content="https://mandalikaputrabersama.com/images/logo.png">
+    <meta property="og:image" content="{{ asset('logo.png') }}">
     <meta property="og:url" content="https://mandalikaputrabersama.com">
     <meta property="og:type" content="website">
 
@@ -27,7 +27,7 @@
     <meta name="twitter:title" content="Mandalika Putra Bersama - Toko Oli Terpercaya di Garut">
     <meta name="twitter:description"
         content="Mandalika Putra Bersama menyediakan oli berkualitas untuk kendaraan Anda di Garut. Kunjungi kami untuk produk terbaik!">
-    <meta name="twitter:image" content="https://mandalikaputrabersama.com/images/logo.png">
+    <meta name="twitter:image" content="{{ asset('logo.png') }}">
     <meta name="twitter:site" content="@mandalikaputra">
 
     <!-- FAVICONS ICON -->
