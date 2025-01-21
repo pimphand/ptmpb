baced5a0e12cf11f1490ff8f3078436f2df8db65
pimphand <faisaldwiki69@gmail.com>
2025-01-21 14:14:03 +0700

    `Added browser_spellcheck, relative_urls, and remove_script_host options to TinyMCE initialization in admin/blog_create.blade.php`

--- resources/views/admin/blog_create.blade.php
+++ resources/views/admin/blog_create.blade.php
@@ -187,9 +187,11 @@
         @else
             tinymce.init({
                 selector: 'textarea',
-
                 plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
                 toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+                browser_spellcheck: true,
+                relative_urls: false,
+                remove_script_host: false,
             });
         @endif
     </script>
